19881e1f959cba8f08205d697869a941ae64f6a2
Jason Chase <jchase@solidmark.com>
2022-12-22 09:53:31 -0500

    Adds basic easing

--- src/Component.svelte
+++ src/Component.svelte
@@ -8,10 +8,16 @@
 
   export let value;
   export let initial = 0;
-  export let duration = 3000;
+  export let duration = 100;
   export let step = 1;
   export let roundto = 1;
   export let format = true;
+	export let ease = 0.5;
+	
+	if (ease > 1) ease = 1;
+	
+	let cur = 0;
+	let thisEase = 0;
 
   function formatNumber(input) {
     if (format) {
@@ -31,8 +37,10 @@
   counterResult[id] = initial;
   timers[id] = setInterval(() => {
     if (isInView) {
-      if (counterResult[id] <= max) {
-        counterResult[id] += step;
+      if (Math.round(cur / roundto) * roundto < max) {
+				thisEase = (max - cur) / ((1 - ease) * 100);
+        counterResult[id] += thisEase;
+				cur = counterResult[id];
       } else {
         clearInterval(timers[id]);
         counterResult[id] = Math.round(max / roundto) * roundto;
